65dddb3513901e6ba53b475afe092b2a0368c81a
agarun <19801205+agarun@users.noreply.github.com>
2017-09-25 01:56:57 -0400

    Add overdue temporary Download skeleton

--- lib/muvy/download.rb
+++ lib/muvy/download.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 require 'youtube-dl.rb'
 
 module Muvy
   class Download
-    #
+    SETTINGS = {}
+
+    attr_reader :media, :options
+
+    def initialize(media, options = {})
+      @media = media
+      @options = options
+      puts "I'm testing that this works properly (Download)"
+    end
   end
 end
