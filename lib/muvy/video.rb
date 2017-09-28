987e769febc867ac91bc112fa351274196e4cfc0
agarun <19801205+agarun@users.noreply.github.com>
2017-09-28 03:21:34 -0400

    Create thumbnail generation in Video

--- lib/muvy/video.rb
+++ lib/muvy/video.rb
@@ -2,17 +2,38 @@ require 'streamio-ffmpeg'
 
 module Muvy
   class Video
-    DEFAULTS = {}
-
-    attr_reader :media, :options
+    attr_reader :media, :options, :settings, :vid
 
     def initialize(media, options = {})
       @media = media
       @options = options
+      @settings = merge_settings
     end
 
     def run
-      puts "Video - test"
+      @vid = FFMPEG::Movie.new(media)
+      screenshots
+      clean
+    end
+
+    # defaults holds default values
+    # options holds command-line arguments
+    # settings merges defaults with options where appropriate
+    def merge_settings
+      defaults = {
+        vframes: options[:fps] * options[:media_length],
+        frame_rate: options[:fps] / Math.sqrt(options[:media_length])
+      }
+
+      @settings = defaults.merge!(options.select { |k| defaults.key?(k) })
+    end
+
+    def screenshots
+      vid.screenshot("#{options[:path]}humb%d.png", settings, validate: false)
+    end
+
+    def clean
+      FileUtils.remove_dir(File.dirname(options[:path]))
     end
   end
 end
