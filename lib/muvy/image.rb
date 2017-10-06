2b33e749b3d93db88aba19e1213c8101b077a69c
agarun <19801205+agarun@users.noreply.github.com>
2017-10-06 03:40:14 -0400

    Add image rotate function

--- lib/muvy/image.rb
+++ lib/muvy/image.rb
@@ -12,11 +12,15 @@ module Muvy
     def run
       montage
       resize
+      rotate
+      printout
     end
+
+    private
 
     def montage
       MiniMagick::Tool::Montage.new do |montage|
-        montage << (media + "thumb*.png")
+        montage << (media + "/thumb*.png")
         montage.mode("Concatenate")
         montage.tile("x1")
         options[:img] =
@@ -28,9 +32,30 @@ module Muvy
       end
     end
 
+    # TODO: Shouldn't make a default for height but instead depend on vid data
+    # TODO: equivalently --> make an option to override youtube-dl default
+    # TODO: Refactor helper functions into classes
     def resize
       img = MiniMagick::Image.new(options[:img])
       img.resize "#{img.width}x#{options[:height]}!"
+    end
+
+    def rotate
+      if options[:rotate]
+        img = MiniMagick::Image.new(options[:img])
+        img.rotate(90)
+      end
+    end
+
+    def printout
+      puts "Saved to #{options[:img]}."
+      puts <<~PRINTOUT if options[:fps]
+
+        Thumbnails made at #{options[:frame_rate] ? options[:frame_rate] : (options[:fps] / (options[:media_length]**(1 / 1.99))).round(4) } frames per second.
+        Original video duration was #{options[:media_length]}.
+        #{'Start time: ' + options[:start].to_s if options[:start]}
+        #{'End time: ' + options[:end].to_s if options[:end]}
+      PRINTOUT
     end
   end
 end
