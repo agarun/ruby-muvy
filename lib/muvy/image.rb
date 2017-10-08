7a49589e843eae4622997883f4b0a05469f75af0
agarun <19801205+agarun@users.noreply.github.com>
2017-10-08 14:23:27 -0400

    Add new helper method Image#modification

--- lib/muvy/image.rb
+++ lib/muvy/image.rb
@@ -11,8 +11,7 @@ module Muvy
 
     def run
       montage
-      resize
-      rotate
+      modification
       printout
     end
 
@@ -32,22 +31,24 @@ module Muvy
       end
     end
 
-    # Arbitrary default height 720
-    def resize
-      img = MiniMagick::Image.new(options[:img])
+    def modification
+      image = MiniMagick::Image.new(options[:img])
 
+      resize(image)
+      rotate(image)
+    end
+
+    # Arbitrary default height 720
+    def resize(image)
       if options[:style] != "stretch"
-        img.resize "#{img.width}x#{options[:height] ||= 720}!"
+        image.resize "#{image.width}x#{options[:height] ||= 720}!"
       elsif options[:height] # and style is stretch
-        img.resize "#{img.width}x#{options[:height]}!"
+        image.resize "#{image.width}x#{options[:height]}!"
       end
     end
 
-    def rotate
-      if options[:rotate]
-        img = MiniMagick::Image.new(options[:img])
-        img.rotate(90)
-      end
+    def rotate(image)
+      image.rotate(90) if options[:rotate]
     end
 
     def printout
