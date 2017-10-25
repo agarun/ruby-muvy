4cd2ffb3f34b7c0010cd5e06916a38af45d37eb0
agarun <19801205+agarun@users.noreply.github.com>
2017-10-24 22:49:13 -0400

    Add Rspec plans

--- spec/muvy_spec.rb
+++ spec/muvy_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe Muvy do
       end.to output(/Muvy::Errors::NoMediaInput/).to_stderr
     end
 
+    # https://stackoverflow.com/questions/1480537/how-can-i-validate-exits-and-aborts-in-rspec
     # it 'should raise exception if it had invalid input' do
     #   expect { Muvy::CLI.new.start }.to raise_error(SystemExit)
     # end
@@ -30,18 +31,33 @@ RSpec.describe Muvy do
   end
 
   # TODO:
-  # 'complains about invalid input'
-  # 'sets path to pwd if no --path given'
-  # 'Media if valid file'
-  # 'ImageFolder and Image if given image folder'
-  # 'Download, Video & Image if given URL'
-  # 'Video and Image if given local file'
+  # 'complain about invalid input'
+  # 'complain about invalid option'
+  # 'make and delete a temporary directory'
+  # 'set path to pwd if no --path given'
+  # 'create instance of Download if input is URL'
+  # 'create instance of Video if input is video'
+  # 'create instance of ImageFolder if input is image folder'
+  # 'exit on invalid URL'
+  # 'exit on invalid local file'
+  # 'exit on invalid folder path'
   # 'downloads youtube video'
-  # 'thumbnails for any video'
-  # 'files in system's temporary folder'
-  # 'rotates an image'
-  # 'rotates an image with a gradient applied'
-  # 'a folder in Dir.pwd or --path'
-  # 'image statistics to stdout'
-  # 'video statistics to stdout true when video given, false when image given'
+  # 'youtube video has 480px height with format 135'
+  # 'downloading creates instance of Video'
+  # 'add :fps and :media_length to options hash in Video#add_options'
+  # 'reads duration of local video file
+  # 'reads fps of local video file
+  # 'create thumbnails in Video#thumbs'
+  # 'reads an image folder into an array'
+  # 'montage images in date order'
+  # 'default image folder montage output height is 720px (--style solid)'
+  # 'exit if input image folder, --style stretch, but --height absent'
+  # 'create final image in --path'
+  # 'resize an image to --height'
+  # 'apply gradient to image file'
+  # 'rotate an image (reverses WxH)'
+  # 'rotate an image with a gradient applied'
+  # 'prints image statistics to stdout'
+  # 'prints only image statistics when no video given'
+  # 'prints video statistics to stdout when video given'
 end
