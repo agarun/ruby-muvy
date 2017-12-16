d0b94c4f2f2e6f21753d8b1f12c5522dbb4ad771
agarun <19801205+agarun@users.noreply.github.com>
2017-12-15 23:07:46 -0500

    Update comments

--- spec/muvy_spec.rb
+++ spec/muvy_spec.rb
@@ -7,57 +7,89 @@ RSpec.describe Muvy do
     end
   end
 
-  describe "initial argument checks" do
-    it 'should raise exception and exit if it had no input' do
-      expect { Muvy::CLI.new.start }.to raise_error(SystemExit)
+  describe "CLI" do
+    describe "initial argument checks" do
+      it "should raise exception and exit if it had no input" do
+        expect { Muvy::CLI.new.start }.to raise_error(SystemExit)
+      end
+
+      it "should print usage & err to stderr after exiting with no input" do
+        expect do
+          begin Muvy::CLI.new.start
+          rescue SystemExit
+          end
+        end.to output(/Muvy::Errors::NoMediaInput/).to_stderr
+      end
+
+      # https://stackoverflow.com/questions/1480537/how-can-i-validate-exits-and-aborts-in-rspec
+      # it "should raise exception if it had invalid input" do
+      #   expect { Muvy::CLI.new.start }.to raise_error(SystemExit)
+      # end
+
+      before do
+        suppress_log_output
+      end
     end
 
-    it 'should print usage & err to stderr after exiting with no input' do
-      expect do
-        begin Muvy::CLI.new.start
-        rescue SystemExit
-        end
-      end.to output(/Muvy::Errors::NoMediaInput/).to_stderr
+    it "complains about invalid input"
+    it "complains about invalid option"
+    it "makes a temporary directory"
+    it "deletes the temporary directory"
+    it "set path to pwd if no --path given"
+
+  end
+
+  describe "Media" do
+    context "URL input" do
+      it "creates instance of Download if input is URL"
+      it "raises error on invalid URL"
     end
 
-    # https://stackoverflow.com/questions/1480537/how-can-i-validate-exits-and-aborts-in-rspec
-    # it 'should raise exception if it had invalid input' do
-    #   expect { Muvy::CLI.new.start }.to raise_error(SystemExit)
-    # end
+    context "Video input" do
+      it "creates instance of Video if input is video"
+      it "raises error on invalid local file"
+    end
 
-    before do
-      suppress_log_output
+    context "ImageFolder input" do
+      it "creates instance of ImageFolder if input is image folder"
+      it "raises error on invalid folder path"
     end
   end
 
-  # TODO:
-  # 'complain about invalid input'
-  # 'complain about invalid option'
-  # 'make and delete a temporary directory'
-  # 'set path to pwd if no --path given'
-  # 'create instance of Download if input is URL'
-  # 'create instance of Video if input is video'
-  # 'create instance of ImageFolder if input is image folder'
-  # 'exit on invalid URL'
-  # 'exit on invalid local file'
-  # 'exit on invalid folder path'
-  # 'downloads youtube video'
-  # 'youtube video has 480px height with format 135'
-  # 'downloading creates instance of Video'
-  # 'add :fps and :media_length to options hash in Video#add_options'
-  # 'reads duration of local video file
-  # 'reads fps of local video file
-  # 'create thumbnails in Video#thumbs'
-  # 'reads an image folder into an array'
-  # 'montage images in date order'
-  # 'default image folder montage output height is 720px (--style solid)'
-  # 'exit if input image folder, --style stretch, but --height absent'
-  # 'create final image in --path'
-  # 'resize an image to --height'
-  # 'apply gradient to image file'
-  # 'rotate an image (reverses WxH)'
-  # 'rotate an image with a gradient applied'
-  # 'prints image statistics to stdout'
-  # 'prints only image statistics when no video given'
-  # 'prints video statistics to stdout when video given'
+  describe "Download" do
+    it "downloads youtube video"
+    it "youtube video has 480px height with format 135 by default"
+    it "downloading creates instance of Video"
+  end
+
+  describe "Video" do
+    describe "#add_options" do
+      it "add :fps and :media_length to options hash"
+    end
+
+    describe "#thumbs" do
+      it "creates thumbnails"
+    end
+
+    it "reads duration of local video file"
+    it "reads fps of local video file"
+  end
+
+  describe "ImageFolder" do
+    it "reads an image folder into an array"
+    it "montage images in date order"
+    it "default image folder montage output height is 720px (--style solid)"
+    it "exits if input image folder, --style stretch, but --height absent"
+  end
+
+  describe "Image" do
+    it "creates final image in --path"
+    it "resizes an image to --height"
+    it "applies gradient to image file"
+    it "rotate an image (reverses WxH)"
+    it "rotate an image with a gradient applied"
+    it "prints image statistics to stdout"
+    it "prints only image statistics when no video given"
+    it "prints video statistics to stdout when video given"
+  end
 end
